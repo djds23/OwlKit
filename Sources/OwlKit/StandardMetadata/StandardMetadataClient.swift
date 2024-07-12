c24385b15d76a5784fd7147f8a72718e76f23306
Dean Silfen <dean.silfen@gmail.com>
2024-07-12 18:19:29 -0400

    A new challenger approaches

---
+++ Sources/OwlKit/StandardMetadata/StandardMetadataClient.swift
@@ -0,0 +1,13 @@
+//
+//  StandardMetadataClient.swift
+//  
+//
+//  Created by Dean Silfen on 7/12/24.
+//
+
+
+// A metadata client that parses standard meta tags
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/meta/name
+class StandardMetadataClient {
+    
+}
